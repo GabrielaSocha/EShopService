2438da7544c1f8f61d2166fb3fa4e91b03b66fe6
GabrielaSocha <232490@student.uek.krakow.pl>
2025-03-08 19:43:55 +0100

    Add project files.

---
+++ EShopService/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace EShopService.Models
+{
+    public class Category
+    {
+        public int id { get; set; }
+        public string name { get; set; } = default!;
+        public bool deleted { get; set; } = false;
+        public DateTime created_at { get; set; } = DateTime.UtcNow;
+        public Guid created_by { get; set; }
+        public DateTime? updated_at { get; set; }
+        public Guid? updated_by { get; set; }
+    }
+}
